353aab041b39fb6904c10797af710a6cbcbb3d39
Lê Văn Tuân <tuan.lv@agiletechsoftware.com>
2023-10-31 09:25:56 +0700

    init 2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,26 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+
+import { Inter } from 'next/font/google';
+
+import '../styles/globals.scss';
+import '../styles/tailwind.css';
+
+const InterFont = Inter({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  display: 'swap',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        :root {
+          --fontInter: ${InterFont.style.fontFamily};
+        }
+      `}</style>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
